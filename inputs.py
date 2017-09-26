afc743a09f841f1eb9eb90e2fecc19e2e9a2e9f9
Peilin Duan <pduan@TIs-MacBook-Pro.local>
2017-09-26 00:34:14 -0700

    inputs corrected

--- inputs.py
+++ inputs.py
@@ -28,17 +28,17 @@ def _float_feature(ndarr):
 def _int_feature(value):
   return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-def preprocess():
+def convert_npy_to_tfrecords():
   writer = tf.python_io.TFRecordWriter('train.tfrecords')
   i = 0
   for folder in os.listdir(_TRAIN_DATA_PATH):
     if folder == '.DS_Store':
       continue
 
-    rgb_files = ['data/v_CricketShot_g04_c01_flow.npy']
-    flow_files = ['data/v_CricketShot_g04_c01_flow.npy']
-    # rgb_files = os.listdir(_TRAIN_DATA_PATH + '/' + folder + '/rgb')
-    # flow_files = os.listdir(_TRAIN_DATA_PATH + '/' + folder + '/flow')
+    # rgb_files = ['data/v_CricketShot_g04_c01_rgb.npy']
+    # flow_files = ['data/v_CricketShot_g04_c01_flow.npy']
+    rgb_files = os.listdir(_TRAIN_DATA_PATH + '/' + folder + '/rgb')
+    flow_files = os.listdir(_TRAIN_DATA_PATH + '/' + folder + '/flow')
     num_files = len(rgb_files)
 
     for j in range(num_files):
@@ -53,7 +53,6 @@ def preprocess():
       example = tf.train.Example(features=tf.train.Features(feature=feature))
       writer.write(example.SerializeToString())
     i += 1
-    break
   writer.close()
 
 def inputs(batch_size):
@@ -68,23 +67,23 @@ def inputs(batch_size):
   _, data = reader.read(data_queue)
   features = tf.parse_single_example(data, features=feature)
   print features
-  rgb = tf.reshape(feature['rgb'], [_SAMPLE_VIDEO_FRAMES, _IMAGE_SIZE, _IMAGE_SIZE, 3])
-  flow = tf.reshape(feature['flow'], [_SAMPLE_VIDEO_FRAMES, _IMAGE_SIZE, _IMAGE_SIZE, 2])
-  label = tf.cast(feature['label'], tf.int32)
+  rgb = tf.reshape(features['rgb'], [_SAMPLE_VIDEO_FRAMES, _IMAGE_SIZE, _IMAGE_SIZE, 3])
+  flow = tf.reshape(features['flow'], [_SAMPLE_VIDEO_FRAMES, _IMAGE_SIZE, _IMAGE_SIZE, 2])
+  label = tf.cast(features['label'], tf.int32)
   
   rgbs, flows, labels = tf.train.shuffle_batch([rgb, flow, label], 
                                        batch_size=batch_size, 
                                        num_threads=4,
-                                       capacity=1000 + 3 * batch_size,
-                                       min_after_dequeue=1000)
+                                       capacity=3 + batch_size,
+                                       min_after_dequeue=1)
+  return rgbs, flows, labels
 
-if __name__ == '__main__':
-  preprocess()
-  r,f,l = inputs(1)
-  with tf.Session() as sess:
-    coord = tf.train.Coordinator()
-    threads = tf.trian.start_queue_runners(sess, coord)
-    img, flow, label = sess.run([r, f, l])
-    np.save(img, 'image.npy')
-    np.save(flow, 'flow.npy')
-    print label
+# if __name__ == '__main__':
+#   convert_npy_to_tfrecords()
+
+#   r,f,l = inputs(1)
+#   with tf.Session() as sess:
+#     coord = tf.train.Coordinator()
+#     threads = tf.train.start_queue_runners(sess, coord)
+#     img, flow, label = sess.run([r, f, l])
+#     print img
--- train.py
+++ train.py
@@ -1,13 +1,14 @@
 from __future__ import absolute_import
 from __future__ import division
+import os
 
 import numpy as np
 import tensorflow as tf
 
+from inputs import inputs, preprocess, _IMAGE_SIZE, _SAMPLE_VIDEO_FRAMES
 import i3d
 
-_IMAGE_SIZE = 224
-_NUM_CLASSES = 400
+_NUM_CLASSES = 101
 _BATCH_SIZE = 100
 
 _DROPOUT_KEEP_PROB = 0.5
@@ -20,17 +21,27 @@ _CHECKPOINT_PATHS = {
     'flow_imagenet': 'data/checkpoints/flow_imagenet/model.ckpt',
 }
 
+
 def train():
-  rgb_inputs, flow_inputs, labels = inputs(_BATCH_SIZE)
+  print _IMAGE_SIZE
+  # rgb_inputs, flow_inputs, labels = inputs(_BATCH_SIZE)
+  rgb_inputs = tf.placeholder(
+    tf.float32,
+    shape=(1, _SAMPLE_VIDEO_FRAMES, _IMAGE_SIZE, _IMAGE_SIZE, 3))
+  flow_inputs = tf.placeholder(
+    tf.float32,
+    shape=(1, _SAMPLE_VIDEO_FRAMES, _IMAGE_SIZE, _IMAGE_SIZE, 2))
 
   with tf.variable_scope('RGB'):
     rgb_model = i3d.InceptionI3d(
       _NUM_CLASSES, spatial_squeeze=True, final_endpoint='Logits')
     rgb_logits, _ = rgb_model(
-      rgb_input, is_training=True, dropout_keep_prob=_DROPOUT_KEEP_PROB)
+      rgb_inputs, is_training=True, dropout_keep_prob=_DROPOUT_KEEP_PROB)
   rgb_variable_map = {}
   for variable in tf.global_variables():
     if variable.name.split('/')[0] == 'RGB':
+      if 'Logits' in variable.name: # skip the last layer
+        continue 
       rgb_variable_map[variable.name.replace(':0', '')] = variable
   rgb_saver = tf.train.Saver(var_list=rgb_variable_map, reshape=True)
 
@@ -38,50 +49,53 @@ def train():
     flow_model = i3d.InceptionI3d(
         _NUM_CLASSES, spatial_squeeze=True, final_endpoint='Logits')
     flow_logits, _ = flow_model(
-        flow_input, is_training=True, dropout_keep_prob=_DROPOUT_KEEP_PROB)
+        flow_inputs, is_training=True, dropout_keep_prob=_DROPOUT_KEEP_PROB)
   flow_variable_map = {}
   for variable in tf.global_variables():
     if variable.name.split('/')[0] == 'Flow':
+      if 'Logits' in variable.name: # skip the last layer
+        continue 
       flow_variable_map[variable.name.replace(':0', '')] = variable
   flow_saver = tf.train.Saver(var_list=flow_variable_map, reshape=True)
 
-  model_logits = rgb_logits + flow_logits
-  loss = tf.reduce_mean(
-    tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=model_logits))
-  lr = 0.01
-  opt = tf.train.GradientDescentOptimizer(lr)
-  train_op = opt.minimize(loss)
+  # model_logits = rgb_logits + flow_logits
+  # loss = tf.reduce_mean(
+  #   tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=model_logits))
+  # lr = 0.01
+  # opt = tf.train.GradientDescentOptimizer(lr)
+  # train_op = opt.minimize(loss)
 
   # saver for fine tuning
-  saver = tf.train.Saver(max_to_keep=10)
-  ckpt_path = './tmp/ckpt'
-  if not os.path.exists(ckpt_path):
-    os.mkdir(ckpt_path)
+  # saver = tf.train.Saver(max_to_keep=10)
+  # ckpt_path = './tmp/ckpt'
+  # if not os.path.exists(ckpt_path):
+  #   os.mkdir(ckpt_path)
   
-  with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+  # with tf.Session() as sess:
+  #   sess.run(tf.global_variables_initializer())
 
-    ckpt = tf.train.get_checkpoint_state(ckpt_path)
-    if ckpt and ckpt.model_checkpoint_path:
-      print 'Restoring from:', ckpt.model_checkpoint_path
-      saver.restore(sess, ckpt.all_model_checkpoint_paths[-1])
-    else:
-      print 'No checkpoint file found, restoring pretrained weights...'
-      rgb_saver.restore(sess, _CHECKPOINT_PATHS['rgb_imagenet'])
-      flow_saver.restore(sess, _CHECKPOINT_PATHS['flow_imagenet'])
+  #   ckpt = tf.train.get_checkpoint_state(ckpt_path)
+  #   if ckpt and ckpt.model_checkpoint_path:
+  #     print 'Restoring from:', ckpt.model_checkpoint_path
+  #     saver.restore(sess, ckpt.all_model_checkpoint_paths[-1])
+  #   else:
+  #     print 'No checkpoint file found, restoring pretrained weights...'
+  #     rgb_saver.restore(sess, _CHECKPOINT_PATHS['rgb_imagenet'])
+  #     flow_saver.restore(sess, _CHECKPOINT_PATHS['flow_imagenet'])
 
-    # we're going to use queue in inputs(), so we need to start queue runners 
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess, coord)
-    it = 0
-    while it < _MAX_ITER:
-      sess.run(train_op)
-      if i % 1000 == 0:
-        loss_val = sess.run(loss)
-        print 'step %d, loss = %.3f' % (i, loss_val)
-        if i > 0:
-          saver.save(sess, ckpt_path + '/model_ckpt', i)
-      i += 1
+  #   # we're going to use queue in inputs(), so we need to start queue runners 
+  #   coord = tf.train.Coordinator()
+  #   threads = tf.train.start_queue_runners(sess, coord)
+  #   it = 0
+  #   while it < _MAX_ITER:
+  #     if i % 1000 == 0:
+  #       _, loss_val = sess.run([train_op, loss])
+  #       print 'step %d, loss = %.3f' % (i, loss_val)
+  #       if i > 0:
+  #         saver.save(sess, ckpt_path + '/model_ckpt', i)
+  #     else:
+  #       sess.run(train_op)
+  #     i += 1
 
 if __name__ == '__main__':
   train()
