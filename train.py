f95c67975880e5e835c70053dad0b5e78749d2d3
Peilin Duan <peilin2duan@gmail.com>
2017-10-18 14:50:54 -0400

    validation works by using prefetch queue

--- train.py
+++ train.py
@@ -50,7 +50,7 @@ def restore():
 def tower_inference(rgb_inputs, flow_inputs, labels):
   rgb_logits, flow_logits = inference(rgb_inputs, flow_inputs)
   model_logits = rgb_logits + flow_logits
-  return tf.reduce_sum(
+  return tf.reduce_mean(
              tf.nn.sparse_softmax_cross_entropy_with_logits(
                 labels=labels, logits=model_logits)), model_logits
 
@@ -76,8 +76,8 @@ def get_true_counts(tower_logits_labels):
   for logits, labels in tower_logits_labels:
     true_count += tf.reduce_sum(
                    tf.cast(
-                     tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1)),
-                     tf.float32
+                     tf.equal(tf.argmax(logits, 1, output_type=tf.int32), labels),
+                     tf.int32
                    )
                  )
   return true_count
@@ -96,29 +96,29 @@ if __name__ == '__main__':
   tower_logits_labels = []
 
   # prefetch train/val batch
-  train_prefetch_queue = tf.FIFOQueue(capacity=BATCH_SIZE,
+  train_prefetch_queue = tf.PaddingFIFOQueue(capacity=2,
                                 dtypes=[tf.float32, tf.float32, tf.int32], 
-                                shapes=[[NUM_FRAMES, CROP_SIZE, CROP_SIZE, 3],
-                                        [NUM_FRAMES, CROP_SIZE, CROP_SIZE, 2],
-                                        []])
-  val_prefetch_queue = tf.FIFOQueue(capacity=BATCH_SIZE,
+                                shapes=[[None, NUM_FRAMES, CROP_SIZE, CROP_SIZE, 3],
+                                        [None, NUM_FRAMES, CROP_SIZE, CROP_SIZE, 2],
+                                        [None]])
+  val_prefetch_queue = tf.PaddingFIFOQueue(capacity=2,
                               dtypes=[tf.float32, tf.float32, tf.int32], 
-                              shapes=[[NUM_FRAMES, CROP_SIZE, CROP_SIZE, 3],
-                                      [NUM_FRAMES, CROP_SIZE, CROP_SIZE, 2],
-                                      []])
+                              shapes=[[None, NUM_FRAMES, CROP_SIZE, CROP_SIZE, 3],
+                                      [None, NUM_FRAMES, CROP_SIZE, CROP_SIZE, 2],
+                                      [None]])
   train_batch = train_pipeline.get_batch(train=True)
   val_batch = val_pipeline.get_batch(train=False)
-  train_enq = train_prefetch_queue.enqueue_many(train_batch)
+  train_enq = train_prefetch_queue.enqueue(train_batch)
   tf.train.add_queue_runner(tf.train.QueueRunner(train_prefetch_queue, [train_enq]))
-  val_enq = val_prefetch_queue.enqueue_many(val_batch)
+  val_enq = val_prefetch_queue.enqueue(val_batch)
   tf.train.add_queue_runner(tf.train.QueueRunner(val_prefetch_queue, [val_enq]))
   
   with tf.variable_scope(tf.get_variable_scope()):
     for i in range(NUM_GPUS):
       with tf.name_scope('tower_%d' % i):
-        rgbs, flows, labels = tf.cond(is_training, lambda: train_prefetch_queue.dequeue_up_to(BATCH_SIZE), lambda: val_prefetch_queue.dequeue_up_to(BATCH_SIZE))
-        rgbs = tf.reshape(rgbs, [-1, NUM_FRAMES, CROP_SIZE, CROP_SIZE, 3])
-        flows = tf.reshape(flows, [-1, NUM_FRAMES, CROP_SIZE, CROP_SIZE, 2])
+        rgbs, flows, labels = tf.cond(is_training, lambda: train_prefetch_queue.dequeue(), lambda: val_prefetch_queue.dequeue())
+       # rgbs = tf.reshape(rgbs, [-1, NUM_FRAMES, CROP_SIZE, CROP_SIZE, 3])
+       # flows = tf.reshape(flows, [-1, NUM_FRAMES, CROP_SIZE, CROP_SIZE, 2])
         with tf.device('/gpu:%d' % i):
           loss, logits = tower_inference(rgbs, flows, labels)
           tf.get_variable_scope().reuse_variables()
@@ -128,8 +128,7 @@ if __name__ == '__main__':
           tower_logits_labels.append((logits, labels))
   
   true_count_op = get_true_counts(tower_logits_labels)
-  total_loss = tf.reduce_sum(tower_losses)
-  avg_loss = total_loss / (NUM_GPUS * BATCH_SIZE)
+  avg_loss = tf.reduce_mean(tower_losses)
   grads = average_gradients(tower_grads)
   train_op = opt.apply_gradients(grads)
   rgb_saver, flow_saver = restore()
@@ -162,7 +161,8 @@ if __name__ == '__main__':
     prefetch_threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
     summary_writer = tf.summary.FileWriter(LOGDIR, sess.graph)
-
+    tf.logging.set_verbosity(tf.logging.INFO) 
+   
     try:
       it = 0
       last_time = time.time()
@@ -184,7 +184,7 @@ if __name__ == '__main__':
           true_count = 0
           val_loss = 0
           for i in range(0, len(val_pipeline.videos), NUM_GPUS * BATCH_SIZE):
-            c, l = sess.run([true_count_op, total_loss], {is_training: False})
+            c, l = sess.run([true_count_op, avg_loss], {is_training: False})
             true_count += c
             val_loss += l
           # add val accuracy to summary
@@ -195,7 +195,7 @@ if __name__ == '__main__':
           ])
           summary_writer.add_summary(acc_summ, it)
           # add val loss to summary
-          val_loss = val_loss / len(val_pipeline.videos)
+          val_loss = np.mean(val_loss)
           tf.logging.info('val loss: %.3f', val_loss)
           val_loss_summ = tf.Summary(value=[
             tf.Summary.Value(tag="val_loss", simple_value=val_loss)
@@ -207,7 +207,7 @@ if __name__ == '__main__':
           last_time = time.time()
           steps = it - last_step
           last_step = it
-          through_put = int(steps * NUM_GPUS * BAT / duration)
+          through_put = int(steps * NUM_GPUS * BATCH_SIZE / duration)
           tf.logging.info('num examples/sec: %d', through_put)
           through_put_summ = tf.Summary(value=[
             tf.Summary.Value(tag="through_put", simple_value=through_put)
